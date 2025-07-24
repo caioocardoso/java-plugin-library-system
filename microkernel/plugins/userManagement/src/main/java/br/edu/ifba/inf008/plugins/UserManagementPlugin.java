b55da4acb81179632f2694e185e571d7bbeb6186
caioocardoso <caiooliveiracardosoo@gmail.com>
2025-07-24 16:37:23 -0300

    fix: userManagementIcon fixed

--- microkernel/plugins/userManagement/src/main/java/br/edu/ifba/inf008/plugins/UserManagementPlugin.java
+++ microkernel/plugins/userManagement/src/main/java/br/edu/ifba/inf008/plugins/UserManagementPlugin.java
@@ -33,8 +33,11 @@ import javafx.scene.shape.SVGPath;
 import javafx.scene.control.Label;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class UserManagementPlugin implements IPlugin {
+
     private final UserDAO userDAO = new UserDAOImpl();
     private TableView<User> userTable = new TableView<>();
 
@@ -48,23 +51,14 @@ public class UserManagementPlugin implements IPlugin {
         menuItem.setOnAction(e -> showUserManagementTab());
 
         Button usersButton = uiController.addQuickAccessButton("", () -> showUserManagementTab());
-        usersButton.setGraphic(createUsersIcon());
+        Image userIconImage = new Image(getClass().getResourceAsStream("/images/userIcon.png"));
+        ImageView userIconView = new ImageView(userIconImage);
+        userIconView.setFitWidth(48);
+        userIconView.setFitHeight(48);
+
+        usersButton.setGraphic(userIconView);
 
         return true;
-    }
-
-    private Node createUsersIcon() {
-        SVGPath path1 = new SVGPath();
-        path1.setContent(
-                "M458.159,404.216c-18.93-33.65-49.934-71.764-100.409-93.431c-28.868,20.196-63.938,32.087-101.745,32.087   c-37.828,0-72.898-11.89-101.767-32.087c-50.474,21.667-81.479,59.782-100.398,93.431C28.731,448.848,48.417,512,91.842,512   c43.426,0,164.164,0,164.164,0s120.726,0,164.153,0C463.583,512,483.269,448.848,458.159,404.216z");
-        path1.getStyleClass().add("quick-button-icon");
-
-        SVGPath path2 = new SVGPath();
-        path2.setContent(
-                "M256.005,300.641c74.144,0,134.231-60.108,134.231-134.242v-32.158C390.236,60.108,330.149,0,256.005,0   c-74.155,0-134.252,60.108-134.252,134.242V166.4C121.753,240.533,181.851,300.641,256.005,300.641z");
-        path2.getStyleClass().add("quick-button-icon");
-
-        return new Group(path1, path2);
     }
 
     private void showUserManagementTab() {
@@ -74,6 +68,7 @@ public class UserManagementPlugin implements IPlugin {
     }
 
     private VBox createManagementPane() {
+        userTable.getColumns().clear();
         TableColumn<User, String> nameCol = new TableColumn<>("Name");
         nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
 
@@ -104,7 +99,6 @@ public class UserManagementPlugin implements IPlugin {
 
         VBox mainPane = new VBox(10, userTable, buttonBox);
         mainPane.setPadding(new Insets(10));
-        VBox.setVgrow(userTable, Priority.ALWAYS);
 
         return mainPane;
     }
@@ -158,6 +152,10 @@ public class UserManagementPlugin implements IPlugin {
         result.ifPresent(user -> {
             if (user.getName().isEmpty() || user.getEmail().isEmpty()) {
                 showAlert(Alert.AlertType.ERROR, "Validation Error", "Name and Email cannot be empty.");
+                return;
+            }
+            if (!user.getEmail().matches("^[\\w.-]+@[\\w.-]+\\.\\w+$")) {
+                showAlert(Alert.AlertType.ERROR, "Validation Error", "Invalid email format.");
                 return;
             }
             try {
